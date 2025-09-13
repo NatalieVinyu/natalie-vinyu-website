f5e805189cb835350c2de6263b5664d1a86e0508
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-09-13 16:46:50 +0200

    added footer

---
+++ src/Components/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function Footer() {
+  return (
+    <div className='text-white'>
+      <div className='flex justify-center bg-blue-400'>
+        © Natalie Vinyu 2024
+      </div>    
+    </div>
+  )}
