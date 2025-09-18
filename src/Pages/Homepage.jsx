2b79bdecfc8246d81ce725a2141c96e4ff234dce
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-09-18 20:05:13 +0200

    added button

--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -8,12 +8,12 @@ export default function Homepage() {
         <h2 className="text-3xl md:text-7xl text-gray-700">Front-end Developer</h2>
 
         <div className="flex flex-col md:flex-row justify-center gap-6 mt-6">
-          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition">
+          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition cursor-pointer">
             Let's Connect
           </button>
-          <button className="px-6 py-3 border-2 border-blue-600 text-blue-600 rounded-lg hover:bg-blue-100 transition">
+          <a href="https://www.linkedin.com/in/natalie-vinyu" target='_blank' rel="noopener noreferrer" className="px-6 py-3 border-2 border-blue-600 text-blue-600 rounded-lg hover:bg-blue-100  transition cursor-pointer">
             Download CV
-          </button>
+          </a>
         </div>
       </div>
     </div>
