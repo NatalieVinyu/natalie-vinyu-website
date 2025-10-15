79c7e2d8d7671b2ef4952485944705963735632c
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-10-16 00:03:04 +0200

    added images

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 export default function About() {
   return (
     <div id='about' className='container mx-auto min-h-screen flex flex-col justify-center '>
-      <div className=''>
-        <div>
+      <div className='bg-black'>
+        <div className='text-white'>
         <h1 className='text-2xl'>About me</h1>
         <br />
         My journey into web development began out of sheer curiosity, which quickly grew into a full-fledged passion. I’ve spent countless hours honing my skills in HTML, CSS, JavaScript, and React, building everything from simple landing pages to dynamic, responsive web applications. I’m always eager to learn and embrace new challenges, and I thrive in environments where creativity meets technology. I’m excited to bring my self-driven, problem-solving mindset to new projects and collaborate with like-minded individuals to create impactful digital experiences.
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,16 +2,17 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-50">
-      <div className="text-center space-y-8">
-        <h1 className="text-6xl md:text-9xl font-bold">Natalie Vinyu</h1>
-        <h2 className="text-3xl md:text-7xl text-gray-700">Front-end Developer</h2>
+    <div className="flex items-center justify-center min-h-screen bg-[url('./assets/hero-bg.jpg')] bg-cover bg-center bg-black/80">
+     <div></div>
+      <div className="text-center space-y-8" >
+        <h1 className="text-6xl md:text-9xl text-white font-bold">Natalie Vinyu</h1>
+        <h2 className="text-3xl md:text-7xl text-white">Front-end Developer</h2>
 
         <div className="flex flex-col md:flex-row justify-center gap-6 mt-6">
-          <button className="px-6 py-3 bg-blue-600 text-white rounded-lg hover:bg-blue-700 transition cursor-pointer">
+          <button className="px-6 py-3 text-white rounded-lg  transition cursor-pointer">
             Download CV
           </button>
-          <a href="https://www.linkedin.com/in/natalie-vinyu" target='_blank' rel="noopener noreferrer" className="px-6 py-3 border-2 border-blue-600 text-blue-600 rounded-lg hover:bg-blue-100  transition cursor-pointer">
+          <a href="https://www.linkedin.com/in/natalie-vinyu" target='_blank' rel="noopener noreferrer" className="px-6 py-3 border-2 text-white rounded-lg transition cursor-pointer">
             Let's connect
           </a>
         </div>
