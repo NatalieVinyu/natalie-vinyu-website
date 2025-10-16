ab1dad09052d4a295f1c00268d22203b87938d3f
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2025-10-16 02:35:57 +0200

    background dim

--- src/Components/nav.jsx
+++ src/Components/nav.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Navbar() {
   return (
     <div className='fixed w-full bg-gray-100 bg-transparent bg-opacity-25 backdrop-blur-md shadow-lg p-8 z-10 top-0'>
-      <nav className='container'>        
+      <nav className='container text-white'>        
           <ul className='flex space-x-20 justify-end'>
             <li>
               <a href='#about'>
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -2,8 +2,7 @@ import React from 'react'
 
 export default function Homepage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[url('./assets/hero-bg.jpg')] bg-cover bg-center bg-black/80">
-     <div></div>
+    <div className="flex items-center justify-center min-h-screen bg-[url('./assets/hero-bg.jpg')] bg-cover bg-center bg-violet-950/90 bg-blend-overlay">
       <div className="text-center space-y-8" >
         <h1 className="text-6xl md:text-9xl text-white font-bold">Natalie Vinyu</h1>
         <h2 className="text-3xl md:text-7xl text-white">Front-end Developer</h2>
