1065ec037874ab25eff4eb21a4fbf6f97aa7450f
Natalie Vinyu <135601983+NatalieVinyu@users.noreply.github.com>
2026-01-19 18:28:38 +0200

    add typewriter effect to homepage and update about section layout

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -22,25 +22,27 @@ export default function About() {
         <div className='w-20 h-1 bg-white mt-2 rounded-full'></div>
       </div>
 
-      <div className='justify-center text-white pt-12'>
-        <p>I'm a web developer with a passion for creating exceptional digital experiences. My journey in web development started with a curiosity about how things work on the web, and has evolved into a career focused on building impactful solutions.</p>
-        <br />
-        <p>I specialize in modern web technologies and love staying up-to-date with the latest trends and best practices. Whether it's crafting pixel-perfect interfaces or architecting robust backend systems, I bring dedication and creativity to every project.</p>
-        <br />
-       <p> When I'm not coding, you'll find me exploring new technologies, contributing to open-source projects, or sharing knowledge with the developer community.</p>
-      </div>
+    <div className='grid grid-cols-2 gap-2 pt-12'>
+        <div className='align-center justify-center text-white pt-12'>
+          <p>I'm a web developer with a passion for creating exceptional digital experiences. My journey in web development started with a curiosity about how things work on the web, and has evolved into a career focused on building impactful solutions.</p>
+          <br />
+          <p>I specialize in modern web technologies and love staying up-to-date with the latest trends and best practices. Whether it's crafting pixel-perfect interfaces or architecting robust backend systems, I bring dedication and creativity to every project.</p>
+          <br />
+          <p> When I'm not coding, you'll find me exploring new technologies, contributing to open-source projects, or sharing knowledge with the developer community.</p>
+        </div>
 
-      {/* HIGHLIGHTS */}
-      <div className='text-white pt-12'>
-        <div className=''>
-          {highlights.map((item, index) => (
-            <div className='border-2 border-solid rounded-xl m-10 p-5 ' key={index}>
-              <h3 className=''>{item.title}</h3>
-              <p>{item.description}</p>
+
+        <div className='text-white '>
+          <div className=''>
+            {highlights.map((item, index) => (
+              <div className='border-2 border-solid rounded-xl m-10 p-5 ' key={index}>
+               <h3 className=''>{item.title}</h3>
+               <p>{item.description}</p>
+          </div>
+           ))}
+          </div>
         </div>
-          ))}
-        </div>
-      </div>
+     </div>
     
     </section>
   )
--- src/Pages/Homepage.jsx
+++ src/Pages/Homepage.jsx
@@ -1,18 +1,44 @@
 import React from 'react' 
+import Typewriter from 'typewriter-effect';
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
+
 
 export default function Homepage() {
   return (
-    <div className="flex items-center justify-center min-h-screen bg-[url('./assets/hero-bg.jpg')] bg-cover bg-center bg-violet-950/90 bg-blend-overlay">
+    <div className="flex items-center justify-center min-h-screen bg-black flex-col space-y-8">
       <div className="text-center space-y-8" >
         <h1 className="text-6xl md:text-9xl text-white font-bold">Natalie Vinyu</h1>
-        <h2 className="text-3xl md:text-7xl text-white">Front-end Developer</h2>
 
-        <div className="flex flex-col md:flex-row justify-center gap-6 mt-6">
+        <h2 className="text-3xl md:text-7xl text-white"><Typewriter
+          options={{
+            strings: ['Software Engineerer', 'Web Developer'],
+            autoStart: true,
+            loop: true,
+            deleteSpeed: 90,
+            cursor: '|',
+            delay: 75,
+          }}
+          />
+        </h2>
+
+        <div className="flex flex-col md:flex-row justify-center gap-6 mt-12">
           <button className="px-6 py-3 text-white rounded-lg  transition cursor-pointer">
             Download CV
           </button>
           <a href="https://www.linkedin.com/in/natalie-vinyu" target='_blank' rel="noopener noreferrer" className="px-6 py-3 border-2 text-white rounded-lg transition cursor-pointer">
             Let's connect
+          </a>
+        </div>
+
+        <div className='mt-12'>
+          <a href="">
+            <FaGithub className="text-white text-3xl mx-4 inline-block hover:text-gray-300 transition cursor-pointer" />
+          </a>
+          <a href="">
+            <FaLinkedin className="text-white text-3xl mx-4 inline-block hover:text-gray-300 transition cursor-pointer" />
+          </a>
+          <a href="mailto:natalievinyu@gmail.com">
+            <FaEnvelope className="text-white text-3xl mx-4 inline-block hover:text-gray-300 transition cursor-pointer" />
           </a>
         </div>
       </div>
